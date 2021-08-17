061be54d5f78bc4785ac025e1f94fe494bb22845
Maksym Rakomin <maksym.rakomin@gmail.com>
2021-08-17 13:19:26 +0300

    initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <main className="main">
+            <div>
+                <img
+                    src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
+                    alt="..."
+                />
+            </div>
+
+            <div>
+                <img
+                    src="https://assets-global.website-files.com/6005fac27a49a9cd477afb63/60576840e7d265198541a372_bavassano_homepage_gp.jpg"
+                    alt="...."
+                />
+                description
+            </div>
+
+            <div>
+                my Posts
+                <div>new post</div>
+            </div>
+
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+                <div>post 3</div>
+            </div>
+
+            main content
+        </main>
+    )
+}
+
+export default Profile
